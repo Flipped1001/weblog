3a7d1c70097d49a362621728851cdbec52809885
flipped1001 <3154147351@qq.com>
2024-07-16 20:40:05 +0800

    1

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,6 +1,6 @@
 import { defineClientConfig } from 'vuepress/client';
 import { setupTransparentNavbar } from 'vuepress-theme-hope/presets/transparentNavbar.js';
-import { setupSnowFall } from 'vuepress-theme-hope/presets/SnowFall.js';
+import { setupSnowFall } from 'vuepress-theme-hope/presets/snowFall.js';
 import 'vuepress-theme-hope/presets/bounce-icon.scss';
 import 'vuepress-theme-hope/presets/shinning-feature-panel.scss';
 
