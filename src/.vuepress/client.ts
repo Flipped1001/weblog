6a01b0a8756f1755a16812268de82fff93530038
flipped1001 <3154147351@qq.com>
2024-07-19 13:23:09 +0800

    1

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -3,10 +3,18 @@ import { setupTransparentNavbar } from 'vuepress-theme-hope/presets/transparentN
 import { setupSnowFall } from 'vuepress-theme-hope/presets/snowFall.js';
 import 'vuepress-theme-hope/presets/bounce-icon.scss';
 import 'vuepress-theme-hope/presets/shinning-feature-panel.scss';
+import { defineAsyncComponent } from 'vue';
+
+
+const BlogBg = defineAsyncComponent(() => import('./components/BlogBg.vue'));
 
 export default defineClientConfig({
   setup: () => {
     setupTransparentNavbar({ type: 'homepage' });
     setupSnowFall();
   },
+
+  rootComponents:[
+    BlogBg
+  ]
 });
